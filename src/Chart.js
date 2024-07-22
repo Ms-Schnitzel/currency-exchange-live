97f5879247b507ed2c14d1b148f22ba185b2bac5
Ms-Schnitzel <zacnielsen72@gmail.com>
2024-07-21 22:42:42 -0600

    state update

--- src/Chart.js
+++ src/Chart.js
@@ -1,2 +1,9 @@
-import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Dropdown from './Dropdown.js';
+import Chart from 'chart.js/auto';
+
+function Chart() {
+
+}
--- src/Rates.js
+++ src/Rates.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Dropdown from './Dropdown.js'
@@ -5,73 +8,28 @@ import {useState, useEffect} from 'react';
 
 export default function Rates() {
 
-  let ratesTo = {
-    index: []
-  };
-  
+  const [base, setBase] = useState("usd");
+  const [rates, setRates] = useState([]);
+
+
   useEffect(() => {
-    const apiPull = async () => {
-      fetch('https://api.frankfurter.app/latest?from=USD').then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error('Request was either a 404 or 500');
-      }).then((data) => {
-        // console.log("json response: ", data);
-  
-        ratesTo = data.rates;
-        ratesTo.index = Object.keys(ratesTo);
-        rateDisplay();
-        return ratesTo;
-  
-      }).catch((error) => {
-        console.log(error);
-      })
-    }
-
     apiPull();
-  }, []);  
+    // console.log("rates in effect: ", rates);
+  }, [base]);
 
-  const rateDisplay = () => {
-    console.log(ratesTo.index.length);
-    let rootTable = document.getElementById("rate-root");
-    for (let i = 0; i < ratesTo.index.length; i++) {
-      let indexName = ratesTo.index[i];
-      let indexVal = ratesTo[indexName];
-      let newDiv = document.createElement("div");
-      let pInName = document.createElement("p");
-      let nameText = document.createTextNode(`${indexName}`);
-      let pInVal = document.createElement("p");
-      let valText = document.createTextNode(`${indexVal}`);
 
-      pInName.classList.add("col-6", "border", "border-2", "border-black", "bg-success", "bg-opacity-25");
-      pInName.append(nameText);
-      pInVal.classList.add("col-6", "border", "border-2", "border-black", "bg-success", "bg-opacity-25");
-      pInVal.append(valText);
-      
-      newDiv.append(pInName, pInVal);
-      newDiv.classList.add("col-12", "col-lg-8", "d-inline-flex", "flex-row", "mx-auto", "px-3", "py-0");
-
-      rootTable.append(newDiv);
-    }
-
+  function Table() {
+    const listElements = rates.map(item => <li className="d-flex flex-row justify-content-between border border-2 border-dark rounded-2 my-2 p-1 bg-secondary" key={item.country}><div className="col-6 border border-2 border-dark bg-light text-center">{item.country}</div><div className="col-6 border border-2 border-dark bg-light text-center">{item.rate}</div></li>);
+    return (
+      <ul className="">
+        {listElements}
+      </ul>
+    )
   }
 
-  // apiPull();
-  rateDisplay();
 
-  const rateClear = (e) => {
-    let tableRoot = document.getElementById('rate-root');
-    tableRoot.innerHTML = "";
-  }
-
-  const rateUpdate = () => {
-
-    let tableSet = document.querySelector('#rates-table select');
-
-    console.log("tableSet: ", tableSet);
-
-    fetch(`https://api.frankfurter.app/latest?from=${tableSet.value}`).then((response) => {
+  const apiPull = async () => {
+    fetch(`https://api.frankfurter.app/latest?from=${base}`).then((response) => {
       if (response.ok) {
         return response.json();
       }
@@ -79,28 +37,50 @@ export default function Rates() {
     }).then((data) => {
       // console.log("json response: ", data);
 
-      ratesTo = data.rates;
-      ratesTo.index = Object.keys(ratesTo);
+      let rateArray = [];
+      for (let [country, rate] of Object.entries(data.rates)) {
+        let newRate = {
+          country: country,
+          rate: rate
+        };
+        // console.log(newRate);
+        rateArray = [...rateArray, newRate];
+      }
+      // console.log("Array: ", rateArray);
+      const newData = [...rateArray];
 
-      rateClear();
-      rateDisplay();
-      return ratesTo;
+      setRates(newData);
+      return ;
 
     }).catch((error) => {
       console.log(error);
     })
   }
+
+  
+  const handleBaseUpdate = () => {
+    let val = document.querySelector("#table-drop select");
+    apiPull();
+    setBase(val.value);
+    // console.log(rates);
+  }
+
   
   return (
     <main>
       <div className="d-flex flex-column align-items-center" id="rates-table">
         <div 
           className="col-6 m-3"
-          onChange={() => rateUpdate()}
+          id="table-drop"
+          onChange={() => handleBaseUpdate()}
         >
           <Dropdown />
         </div>
-        <div className="row d-flex flex-row border-test col-12 scroll pt-3 border bg-black bg-opacity-25 border-3 rounded-3 border-success" id="rate-root">
+        <div 
+          className="row d-flex flex-row border-test col-12 scroll pt-3 border bg-black bg-opacity-25 border-3 rounded-3 border-success" 
+          id="rate-root"
+        >
+          <Table />
         </div>
       </div>
     </main>
